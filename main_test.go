aaab26e171cccf58e9bf62aa9a133f41dcffc435
MichaelKerscher <63649600+MichaelKerscher@users.noreply.github.com>
2025-04-18 20:31:39 +0200

    Update main_test.go
    update tableCreationQuery to include the category column

--- main_test.go
+++ main_test.go
@@ -44,8 +44,10 @@ const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
     id SERIAL,
     name TEXT NOT NULL,
     price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    category TEXT,
     CONSTRAINT products_pkey PRIMARY KEY (id)
 )`
+
 
 func TestEmptyTable(t *testing.T) {
 	clearTable()
